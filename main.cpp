83beebe8a32a6c374b1decd22d2251fc304c1953
Sidney <sid.14boys@yahoo.fr>
2023-05-12 17:36:07 -0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 
-int main() {
+int main()
+{
   // Create a graph of nodeCount nodes (nodes are represented by ints)
   int nodeCount = 3;
-  int **Graph = new int*[nodeCount];
+  int **Graph = new int *[nodeCount];
 
   int num = 1; // Just for printing, not an accurate description of node
   // For every node, create a node list
-  for (int i = 0; i < nodeCount; i++) {
+  for (int i = 0; i < nodeCount; i++)
+  {
     Graph[i] = new int[i + 1]; // Graph[i] is a node list
-    *Graph[i] = num; // Assigning first node in Graph[i] an arbitrary value
+    *Graph[i] = num;           // Assigning first node in Graph[i] an arbitrary value
     num++;
 
     // Assigning an arbitrary value for every subsequent node in Graph[i]
-    for (int j = 1; j < i + 1; j++) {
+    for (int j = 1; j < i + 1; j++)
+    {
       Graph[i][j] = num;
       num++;
     }
   }
 
   // Printing out the graph.  Again, printed values are arbitrary
-  for (int i = 0; i < nodeCount; i++) {
-    for (int j = 0; j < i + 1; j++) {
-      if (j == 0) {
+  for (int i = 0; i < nodeCount; i++)
+  {
+    for (int j = 0; j < i + 1; j++)
+    {
+      if (j == 0)
+      {
         std::cout << Graph[i][j];
-      } else {
+      }
+      else
+      {
         std::cout << " -> " << Graph[i][j];
       }
     }
@@ -33,3 +41,5 @@ int main() {
 
   return 0;
 }
+
+// testing
