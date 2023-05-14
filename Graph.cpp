5f4fda6518905f7db40b60bf5abf557460d04dd8
Kiran <Kiran2x15@gmail.com>
2023-05-14 01:22:52 -0700

    addNode done

--- Graph.cpp
+++ Graph.cpp
@@ -37,7 +37,36 @@ Graph::~Graph() {}
 Graph::Graph(const Graph &clone) {}
 Graph Graph::operator=(const Graph &rhs) {}
 
-void Graph::addNode(int newNode) {}
+void Graph::addNode(int newNode) {
+  try{
+    // Check if newNode is greater than nodeCount
+    if(newNode <= nodeCount){
+      throw std::invalid_argument("New node must be greater than nodeCount");
+    }
+    // Allocate memory for new node
+    Node *newNode = new Node;
+    // Set value of new node to newNode
+    newNode->setValue(newNode);
+    // Set next of new node to nullptr because it is at the end of the list
+    newNode->setNext(nullptr);
+    // Add new node to adj_list, this should always be right because if the node count is 0 then the new node will be at index 0 and then it will increment to 1 and so on
+    adj_list[nodeCount] = newNode;
+    // Increment nodeCount
+    nodeCount++;
+  }
+  //Catch invalid argument
+  catch(std::invalid_argument &e){
+    std::cout << e.what() << std::endl;
+  }
+  //Catch bad alloc
+  catch(std::bad_alloc &e){
+    std::cout << e.what() << std::endl;
+  }
+  //Catch unknown error
+  catch(...){
+    std::cout << "Unknown error" << std::endl;
+  }
+}
 void Graph::addEdge(int u, int v) {}
 
 Graph Graph::getBFS(int v) {}
