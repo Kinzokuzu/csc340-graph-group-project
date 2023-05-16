45581ee3d1829e6eed337fbe1cb4f9e27c1aaacc
Sidney <sid.14boys@yahoo.fr>
2023-05-16 02:39:30 -0700

    bfs done

--- Graph.h
+++ Graph.h
@@ -3,31 +3,32 @@
 
 #include <iostream>
 
-class Graph {
-  public:
-    Graph();
-    Graph(int s); // Sets this->size = s and allocates memory for s nodes
-    // Big 3
-    ~Graph();                          // Deconstructor
-    Graph(const Graph &clone);         // Copy constructor
-    Graph operator=(const Graph &rhs); // Assignment operator
+class Graph
+{
+public:
+  Graph();
+  Graph(int s); // Sets this->size = s and allocates memory for s nodes
+  // Big 3
+  ~Graph();                          // Deconstructor
+  Graph(const Graph &clone);         // Copy constructor
+  Graph operator=(const Graph &rhs); // Assignment operator
 
-    // Increasing the size of the graph (node count) may require reallocation
-    // of memory
-    void addNode(int newNode);
-    void addEdge(int u, int v);
-    // Returns and prints the breadth-first-search tree (graph) from a given
-    // source node
-    Graph getBFS(int v);
-    // Returns the shortest path from s to v in graph as a list (int*)
-    int* getShortestPath(int s, int v);
+  // Increasing the size of the graph (node count) may require reallocation
+  // of memory
+  void addNode(int newNode);
+  void addEdge(int u, int v);
 
-    void printList();
-    void printGraph();
+  Graph getBFS(int v); // Returns and prints the breadth-first-search tree (graph) from a given  source node
 
-  private:
-    int size;
-    int **adj_list;
+  // Returns the shortest path from s to v in graph as a list (int*)
+  int *getShortestPath(int s, int v);
+
+  void printList();
+  void printGraph();
+
+private:
+  int size;
+  int **adj_list;
 };
 
 #endif
