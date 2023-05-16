c0963e9bc03f84df39e14655c826866c3f35925f
Kinzokuzu <sauljcervantes2@gmail.com>
2023-05-16 16:52:00 -0700

    All constructor tests now pass

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 #include "UnitTest.h"
 
 int main() {
+
+  Graph first(3);
+  first.addEdge(0, 1);
+  first.addEdge(0, 2);
+  first.printGraph();
+
+  Graph second(first);
+  second.printGraph();
+
+  std::cout << first.isEqual(second) << std::endl;
+  std::cout << second.isEqual(first) << std::endl;
+
+  std::cout << "Tests\n";
   // Test Node
   std::cout << test_NodeDefCon() << std::endl;
   std::cout << test_NodeAssignment() << std::endl;
@@ -9,9 +22,9 @@ int main() {
   // Test Graph constructors, de-constructors and assignment operator
   std::cout << test_GraphDefCon() << std::endl;
   std::cout << test_GraphInitCon() << std::endl;
-  //std::cout << test_GraphCopyCon() << std::endl; // Failed
+  std::cout << test_GraphCopyCon() << std::endl;
   std::cout << test_GraphDeCon() << std::endl;
-  //std::cout << test_GraphAssignment() << std::endl; // Failed
+  std::cout << test_GraphAssignment() << std::endl; // Failed
   // Test Graph member functions
   std::cout << test_GraphIsEqual() << std::endl;
   std::cout << test_GraphAddNode() << std::endl;
