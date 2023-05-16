53425ddea5535a153a62b3db282049111165aa81
Kiran <Kiran2x15@gmail.com>
2023-05-16 00:18:20 -0700

    fixing addNode with main and kzk commit

--- Graph.h
+++ Graph.h
@@ -3,48 +3,59 @@
 
 #include <iostream>
 
-class Node {
-  public:
-    void setValue(int val);
-    int getValue();
+class Node
+{
+public:
+  Node(); // Does nothing
+  // Assignment only copies fields and not addresses
+  Node &operator=(const Node &rhs);
+  bool isEqual(const Node &comp);
 
-    void setNext(Node *n);
-    Node* getNext();
+  void setValue(int val);
+  int getValue() const;
 
-    void printList();
+  void setNext(Node *n);
+  Node *getNext();
 
-  private:
-    int value;
-    Node *next;
+  void printList();
+
+private:
+  int value;
+  Node *next;
 }; // End class Node
 
-class Graph {
-  public:
-    Graph();
-    Graph(int s); // Sets this->size = s and allocates memory for s nodes
-    // Big 3
-    ~Graph();                          // Deconstructor
-    Graph(const Graph &clone);         // Copy constructor
-    Graph operator=(const Graph &rhs); // Assignment operator
+class Graph
+{
+public:
+  Graph();         // Sets nodeCount = 0, edgeCount = 0
+  Graph(int size); // Sets nodeCount = size and create size nodes
+  // Big 3
+  ~Graph();                           // De-constructor
+  Graph(const Graph &clone);          // Copy constructor
+  Graph &operator=(const Graph &rhs); // Assignment operator
 
-    // Adds a new node with specified value to adj_list and increments nodeCount
-    // value must be > nodeCount
-    void addNode(int value);
-    // Adds the node with value v to the list headed by node with value u and
-    // increments edgeCount
-    void addEdge(int u, int v);
-    // Returns and prints the breadth-first-search tree (graph) from a given
-    // source node
-    Graph getBFS(int v);
-    // Returns the shortest path from s to v in graph as a list (int*)
-    int* getShortestPath(int s, int v);
+  bool isEqual(const Graph &comp);
+  // Adds a new node with specified value to adj_list and increments nodeCount
+  // value must be > nodeCount
+  void addNode(int value);
+  // Adds the node with value v to the list headed by node with value u and
+  // increments edgeCount
+  void addEdge(int u, int v);
+  // Returns and prints the breadth-first-search tree (graph) from a given
+  // source node
+  Graph getBFS(int v);
+  // Returns and prints the shortest path from s to v in graph as a list (Node*)
+  Node *getShortestPath(int s, int v);
 
-    void printGraph();
+  int getNodeCount() const;
+  int getEdgeCount() const;
 
-  private:
-    int nodeCount; // Holds the total number of nodes in graph.
-    int edgeCount; // Hold the total number of edges in graph
-    Node **adj_list;
+  void printGraph();
+
+private:
+  int nodeCount; // Holds the total number of nodes in graph.
+  int edgeCount; // Hold the total number of edges in graph
+  Node **adj_list;
 }; // End class Graph
 
 #endif
