28d30c410b1793f01b5710627f004a22e8063cc0
Sidney <sid.14boys@yahoo.fr>
2023-05-17 13:10:02 -0700

    BFS minor fix

--- Graph.cpp
+++ Graph.cpp
@@ -31,7 +31,7 @@ Graph Graph::getBFS(int v)
 
         for (int i = 0; i < size; i++)
         {
-            if (adj_list[node][i] == 1 && !visited[i])
+            if (adj_list[node][i].value == 1 && !visited[i])
             {
                 visited[i] = true;         // mark the neighbor as visited
                 q.push(i);                 // enqueue the neighbor
@@ -42,6 +42,7 @@ Graph Graph::getBFS(int v)
 
     return bfsGraph;
 }
+
 int *Graph::getShortestPath(int s, int v) {}
 
 void Graph::printGraph() {}
