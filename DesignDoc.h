72df4eb25c0d0c0faa96226f9584ca84c9d020af
KPoudel04 <97379358+KPoudel04@users.noreply.github.com>
2023-05-18 18:45:53 -0700

    Update DesignDoc.h

--- DesignDoc.h
+++ DesignDoc.h
@@ -28,4 +28,15 @@ Pseudo Code :
 --> Dequeueing a node from the front of the queue. --> Accessing the adjacency list of the dequeued node. --> Iterating over the neighbors of the current node and processing them. --> If a neighbor node has not been visited, mark it as visited, enqueue it, and add an edge between the current node and the neighbor node in the bfsGraph. --> After the BFS traversal is complete, the bfsGraph is returned.
 
 Time Complexity : O(N + E), where N is the number of nodes and E is the number of edges in the graph.
+Implement addNode 
+Psuedo Code:
+input(val)
+  set curr as the current node
+  while curr has a next node-> curr = next node
+  create a new node newNode
+  try:
+    set newNode's value to val
+    set newNode's next to nullptr
+  append the list by adding curr's next pointer to be newNode
+  Time complexity: O(n) with n being how many nodes there are.
 */
