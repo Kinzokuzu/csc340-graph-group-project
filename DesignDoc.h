1eb070b58589540eabcf684214836aef79bb1bf7
Kinzokuzu <sauljcervantes2@gmail.com>
2023-05-18 18:08:46 -0700

    Moved BFS pseudo code from README to DesignDoc.h :

--- DesignDoc.h
+++ DesignDoc.h
@@ -8,3 +8,24 @@
  *
  * Date: 5/18/23
  */
+
+/*
+Implement the breadth first search algorithm
+Pseudo Code :
+
+--> Creating a new graph (bfsGraph) for performing breadth-first search (BFS) traversal.
+
+--> Initializing a boolean vector (visited) to keep track of visited nodes. Initially, all nodes are marked as not visited.
+
+--> Creating a queue (q) for BFS traversal.
+
+--> Marking the starting node (v) as visited and enqueueing it.
+
+--> Performing BFS traversal using a while loop that continues until the queue is empty.
+
+-- Inside the while loop:
+
+--> Dequeueing a node from the front of the queue. --> Accessing the adjacency list of the dequeued node. --> Iterating over the neighbors of the current node and processing them. --> If a neighbor node has not been visited, mark it as visited, enqueue it, and add an edge between the current node and the neighbor node in the bfsGraph. --> After the BFS traversal is complete, the bfsGraph is returned.
+
+Time Complexity : O(N + E), where N is the number of nodes and E is the number of edges in the graph.
+*/
